3f1e28e70660d2ac704042f22f8851d92f9c1ff1
JadFu <84548552+JadFu@users.noreply.github.com>
2023-04-13 23:42:39 -0500

    finishing rewriting code, need testing

---
+++ updateUser.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+require('connect.php');
+
+$editCondition = false;
+if ($_POST && $_POST['formStatus'] == 'updateUser') {
+        // Sanitize user input to escape HTML entities and filter out dangerous characters.
+        $name  = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $info = filter_input(INPUT_POST, 'info', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+    
+        
+        // Build the parameterized SQL query and bind to the above sanitized values.
+        $query     = "UPDATE user SET name = :name, email = :email, info = :info WHERE user_id = :user_id";
+        $statement = $db->prepare($query);
+
+        $statement->bindValue(':name', $name);        
+        $statement->bindValue(':email', $email);
+        $statement->bindValue(':info', $info);
+        $statement->bindValue(':item_id', $item_id, PDO::PARAM_INT);
+        // Execute the INSERT.
+        if($statement->execute()){
+            $editCondition = true;
+        }
+
+    } else {
+
+        $user_id = false; // False if we are not UPDATING or SELECTING.
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>EDIT User</title>
+</head>
+<body>
+    <!-- Remember that alternative syntax is good and html inside php is bad -->
+    <div id="postcard">
+        <?php if($editCondition && $_SESSION['userrole'] === 'admin'):?>
+            <h2><a href="adminManage.php">Go Back To Admin Manage interface</a></h2>
+            <h2><a href="index.php">Go Back To GCH Home page</a></h2>
+        <?php elseif($_SESSION['userrole'] != 'admin'): ?>
+            <h2>Only Admin can make this Change</h2>
+            <h2><a href="index.php">Go Back To GCH Home page</a></h2>
+        <?php else: ?>
+            <h2>Edit Failed</h2>
+            <h2><a href="adminManage.php">Go Back To Admin Manage interface</a></h2>
+            <h2><a href="index.php">Go Back To GCH Home page</a></h2>
+        <?php endif ?>
+    </div>
+</body>
+</html>
