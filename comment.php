c5475fe6649db5cd85a5d0b7b853146aa387ffd5
JadFu <84548552+JadFu@users.noreply.github.com>
2023-04-21 00:18:15 -0500

    add css code

--- comment.php
+++ comment.php
@@ -35,7 +35,8 @@ if ($_POST && $_POST['formStatus'] == 'comment')  {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <title>comments</title>
 </head>
 <body>
 <div id="container">
